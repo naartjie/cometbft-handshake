d430e1ab2057d40791f5dc5b72b0f055c7524655
Marcin Jekot <marcin@got.codes>
2023-12-30 18:36:23 +0100

    add decode_initial_handshake

--- src/bin/handshake.rs
+++ src/bin/handshake.rs
@@ -1,7 +1,8 @@
+use std::{error::Error, slice};
+
 use ed25519_consensus::{SigningKey, VerificationKey};
 use rand_core::OsRng;
-use std::error::Error;
-use tokio::io::AsyncWriteExt;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 
 use curve25519_dalek_ng::{
@@ -40,10 +41,35 @@ pub fn encode_initial_handshake(eph_pubkey: &MontgomeryPoint) -> Vec<u8> {
     buf
 }
 
-pub fn handshake_start(stream: TcpStream, private_key: PrivateKey) -> Result<(), String> {
+pub fn decode_initial_handshake(bytes: &[u8]) -> Result<MontgomeryPoint, &str> {
+    // go implementation
+    // https://github.com/tendermint/tendermint/blob/9e98c74/p2p/conn/secret_connection.go#L315-L323
+    if bytes.len() != 34 || bytes[..2] != [0x0a, 0x20] {
+        return Err("malformed_handshake");
+    }
+
+    let eph_pubkey_bytes: [u8; 32] = bytes[2..].try_into().expect("framing failed");
+
+    Ok(MontgomeryPoint(eph_pubkey_bytes))
+}
+
+pub async fn handshake_start(
+    stream: &mut TcpStream,
+    private_key: PrivateKey,
+) -> Result<(), std::io::Error> {
     let private_key = PrivateKey::generate();
     let local_eph_privkey = Scalar::random(&mut OsRng);
     let local_eph_pubkey = X25519_BASEPOINT * &local_eph_privkey;
+
+    stream.write_all(&encode_initial_handshake(&local_eph_pubkey));
+
+    let mut response_len = 0_u8;
+    stream
+        .read_exact(slice::from_mut(&mut response_len))
+        .await?;
+
+    let mut buf = vec![0; response_len as usize];
+    stream.read_exact(&mut buf).await?;
 
     Ok(())
 }
@@ -52,7 +78,6 @@ pub fn handshake_start(stream: TcpStream, private_key: PrivateKey) -> Result<(),
 pub async fn main() -> Result<(), Box<dyn Error>> {
     let addr = "127.0.0.1:26656";
     let mut stream = TcpStream::connect(addr).await?;
-
     println!("connection to peer opened on {}", addr);
 
     let result = stream.write(b"hello world\n").await;
