c39083bf488c0622559812eb16ba96a8161514b4
Marcin Jekot <marcin@got.codes>
2024-01-03 19:49:48 +0100

    Squashed 'cometbft.go/' content from commit c90a5ea39
    git-subtree-dir: cometbft.go
    git-subtree-split: c90a5ea3994e28665fa5603064dec88aaf7bfa43

---
+++ cmd/cometbft/commands/show_node_id.go
@@ -0,0 +1,26 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/cometbft/cometbft/p2p"
+	"github.com/spf13/cobra"
+)
+
+// ShowNodeIDCmd dumps node's ID to the standard output.
+var ShowNodeIDCmd = &cobra.Command{
+	Use:     "show-node-id",
+	Aliases: []string{"show_node_id"},
+	Short:   "Show this node's ID",
+	RunE:    showNodeID,
+}
+
+func showNodeID(*cobra.Command, []string) error {
+	nodeKey, err := p2p.LoadNodeKey(config.NodeKeyFile())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(nodeKey.ID())
+	return nil
+}
